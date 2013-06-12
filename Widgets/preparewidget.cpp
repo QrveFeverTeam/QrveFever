0f62787134a18899debabbade29eb82c0fc43817
Karol Wach <karolzpoznania@wp.pl>
2013-06-12 10:45:45 +0200

    Renaming user name duplicates

--- Widgets/preparewidget.cpp
+++ Widgets/preparewidget.cpp
@@ -16,8 +16,20 @@ PrepareWidget::~PrepareWidget()
 void PrepareWidget::on_pushButton_clicked()
 {
     QList<UserData> users = ui->users->users();
-    if(users.size())
-        emit play(ui->users->users());
+    if(!users.size())
+        return;
+
+    for(int i = 0; i < users.size(); i++) {
+        UserData user = users[i];
+        int n = 1;
+        for(int j = i + 1; j < users.size(); j++) {
+            UserData& u = users[j];
+            if(user.name == u.name)
+                u.name += "-" + QString::number(n++);
+        }
+    }
+
+    emit play(users);
 }
 
 void PrepareWidget::enablePlay()
